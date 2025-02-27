39f9c96114ec5daab52b891f906d143bba27c797
Farook13 <umarulfarook787@gmail.com>
2025-02-27 13:32:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7197644767:AAF0SiJS4hpT9YDcMnq5nAYjCuoNia3
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5032034594').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Kochu_movie_bot") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '0'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002467149516'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Cinema_galaxy_group2')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 #---------------------------------------------------------------
